287a0c4216791c2a67a610c0bf5b72f65f888c28
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-11-23 23:43:45 +0200

    Added basic puzzle grid rendering
    Adjusted html file to accommodate rendering of a simple puzzle grid
    without any real content yet. Next is to give it minimal styling and
    some js keyboard controls.

--- JS/core.js
+++ JS/core.js
@@ -2,6 +2,18 @@ function buildNxNGrid(n){
     if(n < 1){
         console.error("Warning: Bad grid size:\t" + n);
     }
+    var theTable = $("<table id='puzzleGrid'/>");
+    for(var i = 0; i < n; i++){
+        var thisRow = $("<tr id='row-"+i+"'/>");
+        for(var j = 0; j < n; j++){
+            var thisId = "cell-" + i + "-" + j;
+            thisRow.append($("<td/>")
+                   .attr("id", thisId)
+                   .html(thisId));
+        }
+        theTable.append(thisRow);
+    }
+    $("#puzzleContainer").html(theTable);
     console.warn("Not implemented yet:\tfunction buildNxNGrid()");
 }
 
