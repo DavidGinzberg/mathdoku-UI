bfc9f2344cbd60127340dd88ff10871afea7a77c
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-11-23 23:06:39 +0200

    Wrote core.js
    Added directory and file for core.js. Wrote basic code skeleton
    including error messages and minimal functionality. Not yet ready for
    generating puzzle grids.

---
+++ JS/core.js
@@ -0,0 +1,24 @@
+function buildNxNGrid(n){
+    if(n < 1){
+        console.error("Warning: Bad grid size:\t" + n);
+    }
+    console.warn("Not implemented yet:\tfunction buildNxNGrid()");
+}
+
+function getNumberInput(){
+    inVal = $("#puzzleDimInput").value()
+    if (inVal > 0 && inVal <= 10){
+        return inVal;
+    }
+    return -1
+}
+
+function bindButton(){
+    $("#theButton").on("click", function(){
+        buildNxNGrid(getNumberInput());
+    });
+}
+
+$(document).ready( function(){
+    bindButton();
+});
